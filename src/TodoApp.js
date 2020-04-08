73f45526898d84ab5d24f83353f6394bfa764af5
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-04-08 15:04:38 -0400

    init commit, initalizing files with material ui

---
+++ src/TodoApp.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+
+import { red } from "@material-ui/core/colors";
+
+function Todo() {
+  const initialTodos = [
+    {
+      id: 1,
+      task: "Clean Fishtank",
+      completed: false,
+    },
+    {
+      id: 2,
+      task: "Buy bananas",
+      completed: true,
+    },
+    {
+      id: 3,
+      task: "Walk the dog",
+      completed: false,
+    },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS with HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList todos={todos} />
+    </Paper>
+  );
+}
+
+export default Todo;
+
+// -TodoApp
+//   -TodoForm
+//   -TodoList
+//     -TodoItem
