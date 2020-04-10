f3d7bdd6dcd360b6d6ca63230534b3bfddacb323
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-04-10 12:30:54 -0400

    refactoring props

--- src/TodoList.js
+++ src/TodoList.js
@@ -12,10 +12,8 @@ function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
           {todos.map((todo, i) => (
             <>
               <Todo
-                task={todo.task}
+                {...todo}
                 key={todo.id}
-                id={todo.id}
-                completed={todo.completed}
                 removeTodo={removeTodo}
                 toggleTodo={toggleTodo}
                 editTodo={editTodo}
