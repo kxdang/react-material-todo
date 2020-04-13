5a5e98e6f7dd5a3cc354ddc3bf03a8ee6214ba97
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-04-13 13:19:41 -0400

    adding react.memo for optimizations

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, memo } from "react";
 import useToggleState from "./hooks/useToggleState";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -13,6 +13,7 @@ import { DispatchContext } from "./context/todos.context";
 function Todo({ id, task, completed }) {
   const dispatch = useContext(DispatchContext);
   const [isEditing, toggleIsEditing] = useToggleState();
+
   return (
     <ListItem style={{ height: "64px" }}>
       {isEditing ? (
@@ -47,4 +48,4 @@ function Todo({ id, task, completed }) {
   );
 }
 
-export default Todo;
+export default memo(Todo);
